faf65628a1146b0bfe7c4053b83821d08fe19963
Jahangir Alam <jahangir64r@gmail.com>
2023-11-05 13:50:21 +0600

    feat: Add main screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:ar_flutter_plugin_example/screens/account_screen.dart';
+import 'package:ar_flutter_plugin_example/screens/home_screen.dart';
+import 'package:ar_flutter_plugin_example/screens/qr_scan_screen.dart';
+import 'package:ar_flutter_plugin_example/screens/settings_screen.dart';
+import 'package:ar_flutter_plugin_example/screens/upload_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 import 'examples/debugoptionsexample.dart';
 import 'examples/localandwebobjectsexample.dart';
@@ -16,58 +21,219 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  static const String _title = 'AR Plugin Demo';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await ArFlutterPlugin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class MyApp extends StatelessWidget {
+  final PersistentTabController _controller =
+  PersistentTabController(initialIndex: 2);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(_title),
+      title: 'AR Plugin Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        hideNavigationBarWhenKeyboardShows: true,
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
         ),
-        body: Column(children: [
-          Text('Running on: $_platformVersion\n'),
-          Expanded(
-            child: ExampleList(),
-          ),
-        ]),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+        NavBarStyle.style1, // Choose the nav bar style with this property.
       ),
     );
   }
+
+  List<Widget> _buildScreens() {
+    return [
+      SettingsScreen(),
+      UploadScreen(),
+      HomeScreen(),
+      QrScanScreen(),
+      AccountScreen(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.settings),
+        title: ("Settings"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.upload),
+        title: ("Upload"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: ("Home"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.qr_code_scanner),
+        title: ("QR"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.account_circle),
+        title: ("Account"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      )
+    ];
+  }
 }
+
+
+//
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   String _platformVersion = 'Unknown';
+//   static const String _title = 'AR Plugin Demo';
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     initPlatformState();
+//   }
+//
+//   // Platform messages are asynchronous, so we initialize in an async method.
+//   Future<void> initPlatformState() async {
+//     String platformVersion;
+//     // Platform messages may fail, so we use a try/catch PlatformException.
+//     try {
+//       platformVersion = await ArFlutterPlugin.platformVersion;
+//     } on PlatformException {
+//       platformVersion = 'Failed to get platform version.';
+//     }
+//
+//     // If the widget was removed from the tree while the asynchronous platform
+//     // message was in flight, we want to discard the reply rather than calling
+//     // setState to update our non-existent appearance.
+//     if (!mounted) return;
+//
+//     setState(() {
+//       _platformVersion = platformVersion;
+//     });
+//   }
+//
+//   int _selectedIndex = 2;
+//   static const List<Widget> _pages = <Widget>[
+//     Icon(
+//       Icons.settings,
+//       size: 150,
+//     ),
+//     Icon(
+//       Icons.upload,
+//       size: 150,
+//     ),
+//     Center(
+//       // add 20 demo list view
+//       child: Icon(
+//         Icons.home,
+//         size: 150,
+//       ),
+//     ),
+//     Icon(
+//       Icons.qr_code,
+//       size: 150,
+//     ),
+//     Icon(
+//       Icons.account_circle,
+//       size: 150,
+//     ),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: IndexedStack(
+//           index: _selectedIndex,
+//           children: _pages,
+//         ),
+//         bottomNavigationBar: BottomNavigationBar(
+//           type: BottomNavigationBarType.shifting,
+//           selectedIconTheme: IconThemeData(color: Colors.blueAccent, size: 50),
+//           unselectedIconTheme: IconThemeData(color: Colors.grey, size: 25),
+//           showSelectedLabels: false,
+//           showUnselectedLabels: false,
+//           items: const <BottomNavigationBarItem>[
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.settings),
+//               label: 'Settings',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.upload),
+//               label: 'Upload',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.home),
+//               label: 'Home',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.qr_code_scanner),
+//               label: 'Scan QR',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.account_circle),
+//               label: 'Account',
+//             ),
+//           ],
+//           currentIndex: _selectedIndex,
+//           onTap: _onItemTapped,
+//         ),
+//
+//
+//         // body: Column(children: [
+//         //   Text('Running on: $_platformVersion\n'),
+//         //   Expanded(
+//         //     child: ExampleList(),
+//         //
+//         //   ),
+//         // ]),
+//       ),
+//     );
+//   }
+//
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+// }
 
 class ExampleList extends StatelessWidget {
   ExampleList({Key? key}) : super(key: key);
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  Widget drawSearchBox() {
+    return Padding(
+      padding: EdgeInsetsDirectional.only(top: 16, bottom: 32),
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue.withOpacity(0.5),
+              offset: Offset(0, 2),
+              blurRadius: 4,
+              spreadRadius: 2,
+            ),
+          ],
+        ),
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsetsDirectional.only(
+                    start: 16,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          drawSearchBox(),
+          Expanded(
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 0.9,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+              ),
+              children: [
+                makeBoxButton(
+                  'https://picsum.photos/290?image=1',
+                  'Title 1',
+                  'Description 1',
+                ),
+                makeBoxButton(
+                  'https://picsum.photos/250?image=2',
+                  'Title 2',
+                  'Description 2',
+                ),
+                makeBoxButton(
+                  'https://picsum.photos/250?image=3',
+                  'Title 3',
+                  'Description 3',
+                ),
+                makeBoxButton(
+                  'https://picsum.photos/250?image=4',
+                  'Title 4',
+                  'Description 4',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  makeBoxButton(String url, String title, String description) {
+    return Container(
+      child: InkWell(
+        splashColor: Colors.transparent,
+        focusColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Hero(
+              tag: url,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                  height: 170,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
+                    topLeft: Radius.circular(0),
+                    topRight: Radius.circular(0),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        description,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        onTap: () {
+          print('tapped');
+        },
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            offset: Offset(0, 2),
+            blurRadius: 4,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
